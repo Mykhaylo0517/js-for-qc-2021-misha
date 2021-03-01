4ed6de2ccf16c4fd84fc036983f81c8fcba789a3
Mykhaylo0517 <mtlumak@softserveinc.com>
2021-03-01 22:03:47 +0200

    update all file names

---
+++ src/Homework01/task8.js
@@ -0,0 +1,7 @@
+const oneSecond = 1;
+const qSecondsHour = 60 * oneSecond;
+const qSecondsDay = 24 * qSecondsHour;
+const qSecondsMonth = 30 * qSecondsDay;
+document.writeln("There are ", qSecondsHour, " seconds in a hour");
+document.writeln("There are ", qSecondsDay, " seconds in a day");
+document.writeln("There are ", qSecondsMonth, " seconds in a month");
---
+++ src/hotButtonsVSCode.js
@@ -0,0 +1,20 @@
+// add comments on a line:
+    Ctrl + /
+// go to Num line:
+    Ctrl + G and enter number of line
+// Swap the line with neighboring: 
+    Alt + ↑ / ↓
+// Duplicate a line
+    Shift + Alt + ↓ / ↑
+// Go to the paired brace
+    Ctrl + Shift + \
+// Rename the variable
+    F2
+// Format the document
+    Shift + Alt + F
+// Go to variable declaration
+    F12
+// Enable / disable word wrap
+    Alt + Z
+// Enable Zen mode - without panels (go to back - press Escape)
+    Ctrl + K Z
