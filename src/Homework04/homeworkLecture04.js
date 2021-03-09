bcf22846455773d18319d5d9a38ddfc65305c16e
Mykhaylo0517 <mtlumak@softserveinc.com>
2021-03-09 03:31:21 +0200

    add first task for Lesson-04

--- src/Homework04/homeworkLecture04.js
+++ src/Homework04/homeworkLecture04.js
@@ -2,6 +2,31 @@
 
 // ********************  Task 1 **************************
 
+function calcRectangleArea(widthFun, heightFun) {
+    let square;
+    // check the entered values  --- the values are numbers, not NaN or not negative
+    console.log(typeof Number(widthFun) =="number", typeof Number(heightFun) =="number", 
+            !(Number.isNaN(widthFun)), !(Number.isNaN(heightFun)),
+            (Number(widthFun) * Number(heightFun)) > 0);
+    if (typeof Number(widthFun) =="number" && typeof Number(heightFun) =="number" 
+     && !(Number.isNaN(widthFun)) && !(Number.isNaN(heightFun)) 
+     && (Number(widthFun) * Number(heightFun)) > 0)  {
+        square = Number(widthFun) * Number(heightFun);
+    } else {
+        alert (" - " + "The one or two of the values are not numbers or positive!");
+        throw new Error("The one or two of the values are not numbers or positive!");
+    }
+    return square;
+    }
+    
+// input two positive numbers
+let width;
+let height;
+width = Number(prompt("Input the width of the rectangle: ", ""));
+height = Number(prompt("Input the width of the rectangle: ", ""));
+        
+console.log("Calculation of the square of ​​the rectangle for ", width,
+        " ** ", height, " = ", calcRectangleArea(width, height));
 
 // ********************  Task 2 **************************
 
