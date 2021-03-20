c45fe265752d39aaa6d6a2a6ca4e1c9aed5bfc10
Mykhaylo0517 <mtlumak@softserveinc.com>
2021-03-20 18:28:19 +0200

    add task-01 for for lesson-07

---
+++ src/Homework08_JQuery/examplesLecture08.js
@@ -0,0 +1,10 @@
+// ************************************************************
+// Here are examples of code from a classroom lecture N-07)
+// ************************************************************
+let company ="SoftServe"
+window.alert ();
+
+
+
+
+
