8e6cfde0b2793244a4e7d512c44caa8675a0b304
Mykhaylo0517 <mtlumak@softserveinc.com>
2021-04-09 03:39:52 +0300

    added task-2 for Lesson-12

---
+++ src/Homework12_AJAX/task2/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const url = require('url');
+const static = require('node-static');
+const file = new static.Server('.', {
+  cache: 0
+});
+
+function accept(req, res) {
+  if (req.url == '/text1.txt') { // set data.txt
+    setTimeout(function() {
+      file.serve(req, res);
+    }, 500);  // timeout = 0.5 sec - to simulate processing 
+  } else {
+    file.serve(req, res);
+  }
+}
+
+// ------ run -------
+http.createServer(accept).listen(8080);
+console.log("Server running at http://localhost:8080 ...");
